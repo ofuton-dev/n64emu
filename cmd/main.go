81e89e9969bc6a8e18f011f3215c9f74c4b1712e
Akihiro Otomo <37920078+pokemium@users.noreply.github.com>
2020-10-30 04:36:19 +0900

    read ROM passed by command arg (#1)

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"n64emu/pkg/core"
+	"n64emu/pkg/util"
 	"os"
+	"path/filepath"
 )
 
-var version string
+var (
+	version     string
+	showVersion = flag.Bool("v", false, "show version")
+)
 
 const (
 	title = "n64emu"
@@ -23,13 +30,18 @@ func main() {
 }
 
 func Run() int {
-	var (
-		showVersion = flag.Bool("v", false, "show version")
-	)
 	flag.Parse()
+
 	if *showVersion {
 		fmt.Println(title+":", getVersion())
 		return exitCodeOK
+	}
+
+	path := flag.Arg(0)
+	_, err := readROM(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to read ROM data: %s\n", err)
+		return exitCodeError
 	}
 
 	core.Hello()
@@ -41,4 +53,21 @@ func getVersion() string {
 		return "Develop"
 	}
 	return version
+}
+
+func readROM(path string) ([]byte, error) {
+	if path == "" {
+		return []byte{}, errors.New("please enter ROM file path")
+	}
+
+	extFilter := []string{".z64", ".n64"}
+	if !util.Contains(extFilter, filepath.Ext(path)) {
+		return []byte{}, fmt.Errorf("please enter file which has the following extension: %v", extFilter)
+	}
+
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return []byte{}, errors.New("fail to read file")
+	}
+	return bytes, nil
 }
---
+++ pkg/util/util.go
@@ -0,0 +1,21 @@
+package util
+
+import (
+	"reflect"
+	"strings"
+)
+
+func Contains(list interface{}, target interface{}) bool {
+	if reflect.TypeOf(list).Kind() == reflect.Slice || reflect.TypeOf(list).Kind() == reflect.Array {
+		listvalue := reflect.ValueOf(list)
+		for i := 0; i < listvalue.Len(); i++ {
+			if target == listvalue.Index(i).Interface() {
+				return true
+			}
+		}
+	}
+	if reflect.TypeOf(target).Kind() == reflect.String && reflect.TypeOf(list).Kind() == reflect.String {
+		return strings.Contains(list.(string), target.(string))
+	}
+	return false
+}
