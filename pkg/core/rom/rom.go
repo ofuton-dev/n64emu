d229bf4730dd4b28c02658f0745289b00c71e643
K <kamiyaowl@gmail.com>
2020-11-04 23:04:14 +0900

    fix: removed passing of slice by reference

--- pkg/core/rom/rom.go
+++ pkg/core/rom/rom.go
@@ -126,43 +126,43 @@ func swap(a *byte, b *byte) {
 }
 
 // Restore a byte-swapped array
-func convertByteSwapped(src *[]byte) error {
-	if len(*src)%2 != 0 {
+func convertByteSwapped(src []byte) error {
+	if len(src)%2 != 0 {
 		return errors.New("ROM size not divisible by two")
 	}
 
-	loopCount := len(*src) / 2
+	loopCount := len(src) / 2
 	for i := 0; i < loopCount; i++ {
 		index := i * 2
-		swap(&(*src)[index], &(*src)[index+1])
+		swap(&src[index], &src[index+1])
 	}
 
 	return nil
 }
 
 // Convert little-endian arrays to big-endian arrays
-func convertLittle(src *[]byte) error {
-	if len(*src)%4 != 0 {
+func convertLittle(src []byte) error {
+	if len(src)%4 != 0 {
 		return errors.New("ROM size not divisible by four")
 	}
 
-	loopCount := len(*src) / 4
+	loopCount := len(src) / 4
 	for i := 0; i < loopCount; i++ {
 		index := i * 4
-		swap(&(*src)[index], &(*src)[index+3])
-		swap(&(*src)[index+1], &(*src)[index+2])
+		swap(&src[index], &src[index+3])
+		swap(&src[index+1], &src[index+2])
 	}
 
 	return nil
 }
 
 // Repairing array order
-func repairOrder(src *[]byte) error {
-	if len(*src) < 4 {
+func repairOrder(src []byte) error {
+	if len(src) < 4 {
 		return errors.New("The size is less than 4 bytes")
 	}
 
-	header := (uint32((*src)[0]) << 24) | (uint32((*src)[1]) << 16) | (uint32((*src)[2]) << 8) | (uint32((*src)[3]) << 0)
+	header := (uint32(src[0]) << 24) | (uint32(src[1]) << 16) | (uint32(src[2]) << 8) | (uint32(src[3]) << 0)
 	switch header {
 	case RomHeaderBigEndian:
 		break
@@ -224,7 +224,7 @@ func NewRom(romPath string) (Rom, error) {
 	}
 
 	// Detect identifier. repair rom endian and byte-swapped.
-	if err := repairOrder(&src); err != nil {
+	if err := repairOrder(src); err != nil {
 		return dst, err
 	}
 
