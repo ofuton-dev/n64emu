8dde90474e403e15754f02809d205ae145b6ea6c
bokuweb <bokuweb12@gmail.com>
2020-11-06 00:39:46 +0900

    feat: Impl R type instruction and or (#8)

--- pkg/core/r4300i/inst/decode.go
+++ pkg/core/r4300i/inst/decode.go
@@ -20,67 +20,74 @@ R-Instruction format:
 
 package inst
 
+import "n64emu/pkg/types"
+
 // I-Type Instruction format
 type InstI struct {
 	// src[31:26] 6 bits
-	Opcode byte
+	Opcode types.Byte
 	// src[25:21] 5 bits
-	Rs byte
+	Rs types.Byte
 	// src[20:16] 5 bits
-	Rt byte
+	Rt types.Byte
 	// src[15:0] 16 bits
-	Immediate uint16
+	Immediate types.HalfWord
 }
 
 // J-Type Instruction format
 type InstJ struct {
 	// src[31:26] 6 bits
-	Opcode byte
+	Opcode types.Byte
 	// src[25:0] 26 bits
-	Address uint32
+	Address types.Word
 }
 
 // R-Type Instruction format
 type InstR struct {
 	// src[31:26] 6 bits
-	Opcode byte
+	Opcode types.Byte
 	// src[25:21] 5 bits
-	Rs byte
+	Rs types.Byte
 	// src[20:16] 5 bits
-	Rt byte
+	Rt types.Byte
 	// src[15:11] 5 bits
-	Rd byte
+	Rd types.Byte
 	// src[10:6] 5 bits
-	Sa byte
+	Sa types.Byte
 	// src[5:0] 6 bits
-	Funct byte
+	Funct types.Byte
 }
 
 // Decode I-Type Instruction
-func DecodeI(src uint32) InstI {
+func DecodeI(src types.Word) InstI {
 	return InstI{
-		Opcode:    byte((src >> 26) & 0x3f),
-		Rs:        byte((src >> 21) & 0x1f),
-		Rt:        byte((src >> 16) & 0x1f),
-		Immediate: uint16((src >> 0) & 0xffff),
+		Opcode:    GetOp(src),
+		Rs:        types.Byte((src >> 21) & 0x1f),
+		Rt:        types.Byte((src >> 16) & 0x1f),
+		Immediate: types.HalfWord((src >> 0) & 0xffff),
 	}
 }
 
 // Decode J-Type Instruction
-func DecodeJ(src uint32) InstJ {
+func DecodeJ(src types.Word) InstJ {
 	return InstJ{
-		Opcode:  byte((src >> 26) & 0x3f),
+		Opcode:  GetOp(src),
 		Address: (src >> 0) & 0x03ff_ffff,
 	}
 }
 
 // Decode R-Type Instruction
-func DecodeR(src uint32) InstR {
+func DecodeR(src types.Word) InstR {
 	return InstR{
-		Opcode: byte((src >> 26) & 0x3f),
-		Rs:     byte((src >> 21) & 0x1f),
-		Rt:     byte((src >> 16) & 0x1f),
-		Sa:     byte((src >> 6) & 0x1f),
-		Funct:  byte((src >> 0) & 0x3f),
+		Opcode: GetOp(src),
+		Rs:     types.Byte((src >> 21) & 0x1f),
+		Rt:     types.Byte((src >> 16) & 0x1f),
+		Rd:     types.Byte((src >> 11) & 0x1f),
+		Sa:     types.Byte((src >> 6) & 0x1f),
+		Funct:  types.Byte((src >> 0) & 0x3f),
 	}
+}
+
+func GetOp(src types.Word) types.Byte {
+	return types.Byte((src >> 26) & 0x3f)
 }
--- pkg/core/r4300i/reg/gpr_test.go
+++ pkg/core/r4300i/reg/gpr_test.go
@@ -2,25 +2,26 @@ package reg
 
 import (
 	"fmt"
+	"n64emu/pkg/types"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGPR_WriteRead(t *testing.T) {
-	testData := uint64(0xaa995566_33ccbbdd)
+	testData := types.DoubleWord(0xaa995566_33ccbbdd)
 
 	for i := 0; i < NumOfRegsInGpr; i++ {
 		t.Run(fmt.Sprint(i), func(t *testing.T) {
 			gpr := GPR{}
 
 			// write testData and read verify
-			gpr.Write(i, testData)
-			got := gpr.Read(i)
+			gpr.Write(types.Byte(i), testData)
+			got := gpr.Read(types.Byte(i))
 
 			if i == 0 {
 				// always zero
-				assert.Equal(t, uint64(0x0), got)
+				assert.Equal(t, types.DoubleWord(0x0), got)
 			} else {
 				assert.Equal(t, testData, got)
 			}
