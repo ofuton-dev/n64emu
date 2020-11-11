af77c25759297de3f03351bffdfbfeae888c030b
K <kamiyaowl@gmail.com>
2020-11-12 01:25:05 +0900

    (wip) impl command parse in ram

--- pkg/core/pif/pif.go
+++ pkg/core/pif/pif.go
@@ -4,48 +4,6 @@ MemoryMap:
 	0x1FC00000-0x1FC007BF	PIF_ROM_START
 	0x1FC007C0-0x1FC007FF	PIF_RAM
 
-
-Command structure:
-	[64byte block] at 0xbfc007c0 (1fc007c0)
-	{
-	00 00 00 00 : 00 00 00 00 - 8 bytes
-	00 00 00 00 : 00 00 00 00 - 8 bytes
-	00 00 00 00 : 00 00 00 00 - 8 bytes
-	00 00 00 00 : 00 00 00 00 - 8 bytes
-	00 00 00 00 : 00 00 00 00 - 8 bytes
-	00 00 00 00 : 00 00 00 00 - 8 bytes
-	00 00 00 00 : 00 00 00 00 - 8 bytes
-	00 00 00 00 : 00 00 00 00 - 8 bytes
-	}                       ^^pif status/control byte
-
-	Commands are processed from any byte in pifram. ie: The pif chip steps thru each byte to load commands.
-
-	Each command has a structure like so:
-
-	byte 1 (t) = x number of bytes to send to pif chip
-	byte 2 (r) = x number of bytes to recieve from pif chip
-	byte 3 (c) = Command type
-
-Command Types:
-	| Command |       Description        |t |r |
-	+---------+--------------------------+-----+
-	|   00    |   request info (status)  |01|03|
-	|   01    |   read button values     |01|04|
-	|   02    |   read from mempack slot |03|21|
-	|   03    |   write to mempack slot  |23|01|
-	|   04    |   read eeprom            |02|08|
-	|   05    |   write eeprom           |10|01|
-	|   ff    |   reset                  |01|03|
-
-Status of controller:
-	%X  X  X  X  X  X  X  X  X  X  X  X  X  X  X  X
-		A  B  Z  ST U  D  L  R  ?  ?  PL PR CU CD CL CR
-	A,B,Z,ST    = A,B,Z, Start buttons
-	U,D,L,R     = Joypad directions
-	?,?         = Unknown
-	PL,PR       = Pan left, Pan right buttons
-	CU,CD,CL,CR = C buttons (up,down,left,right)
-
 Reference:
 	- https://web.archive.org/web/20200429103221/http://en64.shoutwiki.com/wiki/Memory_map_detailed#PIF
 	- https://web.archive.org/web/20180809011637/http://en64.shoutwiki.com/wiki/SI_Registers_Detailed#PIF_Usage
@@ -60,9 +18,25 @@ import (
 )
 
 const (
-	PifRomSize       = 0x7c0
-	PifRamSize       = 64
-	PifNumOfChannels = 8
+	PifRomSize = 0x7c0
+	PifRamSize = 64
+	// 0~3: joystick, 4: EEPROM, 5: EEPROM(Option)
+	PifNumOfChannels = 6
+)
+
+// StatusByte: PIF RAM[0x3f]
+type PifStatus types.Byte
+
+const (
+	Idle       = PifStatus(0x00)
+	NewCommand = PifStatus(0x01)
+	Busy       = PifStatus(0x80)
+	// TODO: I'll need to find some more information.
+	//  PifStatus(0x02)
+	//  PifStatus(0x08)
+	//  PifStatus(0x10)
+	//  PifStatus(0x30)
+	//  PifStatus(0xc0)
 )
 
 type ChannelType types.Byte
@@ -78,13 +52,28 @@ const (
 type CommandType types.Byte
 
 const (
-	RequestInfo         = CommandType(0x00) // send:  1 byte,  recv:  3 bytes
-	ReadButtonValues    = CommandType(0x01) // send:  1 byte,  recv:  4 bytes
-	ReadFromMempackSlot = CommandType(0x02) // send:  3 bytes, recv: 33 bytes
-	WriteToMempackSlot  = CommandType(0x03) // send: 35 bytes, recv:  1 byte
-	ReadEeprom          = CommandType(0x04) // send:  2 bytes, recv:  8 bytes
-	WriteEeprom         = CommandType(0x05) // send: 16 bytes, recv:  1 byte
-	Reset               = types.Byte(0xff)  // send:  1 byte,  recv:  3 bytes
+	RequestInfo         = CommandType(0x00)
+	ReadButtonValues    = CommandType(0x01)
+	ReadFromMempackSlot = CommandType(0x02)
+	WriteToMempackSlot  = CommandType(0x03)
+	ReadEeprom          = CommandType(0x04)
+	WriteEeprom         = CommandType(0x05)
+	RTCStatuQuery       = CommandType(0x06)
+	ReadRTCBlock        = CommandType(0x07)
+	WriteRTCBlock       = CommandType(0x08)
+	Reset               = types.Byte(0xff)
+)
+
+// Command processing result
+type CommandResult types.Byte
+
+const (
+	// no error, operation successful.
+	Suscess = CommandResult(0x00)
+	// error, device not present for specified command.
+	DeviceNotPresent = CommandResult(0x80)
+	// error, unable to send/recieve the number bytes for command type.
+	UnableToTransferDatas = CommandResult(0x40)
 )
 
 type PIF struct {
@@ -99,43 +88,93 @@ func (pif *PIF) EmulateBoot() {
 	util.TODO("unimplemented.")
 }
 
-// Parse the contents of the command
-func parseCmd(data []types.Byte) (cmd CommandType, tx, rx types.Byte) {
-	assert.Assert(len(data) > 4, "At least four bytes")
-	return CommandType(data[2]), data[0], data[1]
+// Communicate to joystick
+func (pif *PIF) communicateJoystick(channel types.Byte, txBuf, rxBuf []types.Byte) CommandResult {
+	cmd := CommandType(txBuf[0])
+	assert.Assert((cmd != ReadEeprom) && (cmd != WriteEeprom) && (cmd != RTCStatuQuery) && (cmd != ReadRTCBlock) && (cmd != WriteRTCBlock), "Do not send eeprom commands to joyStick")
+
+	// TODO: Keep a Joystick instance and send commands
+	return DeviceNotPresent
 }
 
-func (pif *PIF) runCmd(offset types.Byte) types.Byte {
-	// parse command
+// Communicate to EEPROM
+func (pif *PIF) communicateEEPROM(channel types.Byte, txBuf, rxBuf []types.Byte) CommandResult {
+	cmd := CommandType(txBuf[0])
+	assert.Assert((cmd != ReadButtonValues) && (cmd != ReadFromMempackSlot) && (cmd != WriteToMempackSlot), "Do not send joystick commands to eeprom")
+
+	// TODO: Keep a EEPROM instance and send commands
+	return DeviceNotPresent
+}
+
+// Run Command on PIF RAM
+// Command Block Format:
+//   tx, rx, [cmd, ...], [a, b, ...]
+//           ^^^^^^^^^^  ^^^^^^^^^^^
+//           |tx buf     |rx buf
+//
+// `rx` byte Format:
+//   { result[7:6], num_of_read_bytes[5:0] }
+func (pif *PIF) runCmd(channel, offset types.Byte) types.Byte {
+	// extract buf
 	data := pif.ram[offset:]
-	// cmd, tx, rx := parseCmd(data)
+	assert.Assert(len(data) > 2, "format error")
+	txLen, rxLen := data[0], (data[1] & 0x3f)
+	assert.AssertNe(txLen, 0, "send tx num of bytes is zero")
 
-	return 8 // TODO:
+	txBuf := data[2 : 2+(txLen-1)]         // [cmd, ...]
+	rxBuf := data[2+txLen : 2+txLen+rxLen] // [a, b, ...]
+
+	// communicate
+	result := DeviceNotPresent
+	switch channel {
+	case 0: // joystick0
+	case 1: // joystick1
+	case 2: // joystick2
+	case 3: // joystick3
+		result = pif.communicateJoystick(channel, txBuf, rxBuf)
+		break
+	case 4: // eeprom
+	case 5: // eeproom(option)
+		result = pif.communicateEEPROM(channel, txBuf, rxBuf)
+		break
+	default: // NC
+		assert.Assert(false, "unexpected channel")
+		break
+	}
+
+	// Write the result to rx byte
+	data[1] |= types.Byte(result)
+
+	// number of bytes read and write from RAM
+	return 2 + txLen + rxLen
 }
 
-func (pif *PIF) update() {
-	offset := types.Byte(0)
+// Update PIF Status
+func (pif *PIF) Update() {
+	// check status bytes
+	// statusByte := pif.ram[len(pif.ram)-1]
+	// TODO: check statusByte
 
-	for offset < PifRamSize; {
-		switch ChannelType(pif.ram[i]) {
+	// scan command in ram
+	channel := types.Byte(0)
+	offset := types.Byte(0)
+	for (offset < PifRamSize) && (channel < PifNumOfChannels) {
+		switch ChannelType(pif.ram[offset]) {
 		case SkipChannel:
 		case DummyData:
 			offset++
+			channel++
 			break
 		case EndOfSetup:
+		case ChannelReset:
 			// abort scan
 			offset = PifRamSize
 			break
-		case ChannelReset:
-			util.TODO("unimplemented")
-			i++
-			break
 		default:
 			// do command
-			arrangeBytes := pif.runCmd(i)
-			i += arrangeBytes
+			arrangeBytes := pif.runCmd(channel, offset)
+			offset += arrangeBytes
 			break
 		}
-
 	}
 }
