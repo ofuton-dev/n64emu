dbb5b9f5e50722e921fa8a8f118ad09c9b773586
bokuweb <bokuweb12@gmail.com>
2020-11-14 00:20:16 +0900

    refactor: use opcode to bytes in cpu test

--- pkg/core/mips/r4300i/cpu/cpu_test.go
+++ pkg/core/mips/r4300i/cpu/cpu_test.go
@@ -1,6 +1,7 @@
 package cpu
 
 import (
+	"encoding/binary"
 	"n64emu/pkg/types"
 	"testing"
 
@@ -46,6 +47,12 @@ func (b *MockBus) SetMemory(offset types.Word, data []types.Byte) {
 	}
 }
 
+func beOpcode2bytes(o types.Word) []types.Byte {
+	bytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(bytes, o)
+	return bytes
+}
+
 func setupCPU(offset types.Word, data []types.Byte) (*CPU, *MockBus) {
 	b := MockBus{}
 	b.SetMemory(offset, data)
@@ -55,12 +62,12 @@ func setupCPU(offset types.Word, data []types.Byte) (*CPU, *MockBus) {
 func TestSLL(t *testing.T) {
 	assert := assert.New(t)
 	// SLL rd=3, rt=2, sa=3
-	cpu, _ := setupCPU(0, []types.Byte{0xC0, 0x18, 0x02, 0x00})
+	cpu, _ := setupCPU(0, beOpcode2bytes(0xC0180200))
 	cpu.gpr.Write(2, 0x2)
 	cpu.RunUntil(5)
 	assert.Equal(types.DoubleWord(0x10), cpu.gpr.Read(3), "should shifted value stored")
 	// SLL rd=3, rt=2, sa=0 with sign extended
-	cpu, _ = setupCPU(0, []types.Byte{0x00, 0x18, 0x02, 0x00})
+	cpu, _ = setupCPU(0, beOpcode2bytes(0x00180200))
 	cpu.gpr.Write(2, 0x00000000FFFFFFFF)
 	cpu.RunUntil(5)
 	assert.Equal(types.DoubleWord(0xFFFFFFFFFFFFFFFF), cpu.gpr.Read(3), "should shifted value stored")
@@ -69,7 +76,7 @@ func TestSLL(t *testing.T) {
 func TestSRL(t *testing.T) {
 	assert := assert.New(t)
 	// SLL rd=3, rt=2, sa=3
-	cpu, _ := setupCPU(0, []types.Byte{0xC2, 0x18, 0x02, 0x00})
+	cpu, _ := setupCPU(0, beOpcode2bytes(0xC2180200))
 	cpu.gpr.Write(2, 0x10)
 	cpu.RunUntil(5)
 	assert.Equal(types.DoubleWord(0x2), cpu.gpr.Read(3), "should shifted value stored")
@@ -78,7 +85,7 @@ func TestSRL(t *testing.T) {
 func TestSRA(t *testing.T) {
 	assert := assert.New(t)
 	// SLL rd=3, rt=2, sa=3
-	cpu, _ := setupCPU(0, []types.Byte{0xC3, 0x18, 0x02, 0x00})
+	cpu, _ := setupCPU(0, beOpcode2bytes(0xC3180200))
 	cpu.gpr.Write(2, 0x00000000FFFFFFFF)
 	cpu.RunUntil(5)
 	assert.Equal(types.DoubleWord(0xFFFFFFFFFFFFFFFF), cpu.gpr.Read(3), "should shifted value stored")
@@ -87,7 +94,7 @@ func TestSRA(t *testing.T) {
 func TestOR(t *testing.T) {
 	assert := assert.New(t)
 	// OR rd=3, rs=1, rt=2
-	cpu, _ := setupCPU(0, []types.Byte{0x25, 0x18, 0x22, 0x00})
+	cpu, _ := setupCPU(0, beOpcode2bytes(0x25182200))
 	cpu.gpr.Write(1, 0x00000000AAAAAAAA)
 	cpu.gpr.Write(2, 0x5555555500000000)
 	cpu.RunUntil(5)
