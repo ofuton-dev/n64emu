163b967a8ead048901dd0e314dbee48f17930f7b
K <kamiyaowl@gmail.com>
2020-11-23 01:38:47 +0900

    (wip) Provide comments on the required implementation with TODO

--- pkg/core/accessory/ctrlpak/ctrlpak.go
+++ pkg/core/accessory/ctrlpak/ctrlpak.go
@@ -4,6 +4,7 @@ import (
 	"n64emu/pkg/core/common/nvmem"
 	"n64emu/pkg/core/joybus"
 	"n64emu/pkg/types"
+	"n64emu/pkg/util"
 	"n64emu/pkg/util/assert"
 	"os"
 )
@@ -11,6 +12,8 @@ import (
 const (
 	// 32KByte Battery-backed RAM
 	RAMSize = 0x8000
+	// CmdId:1byte + { block_offset[15:5] ,crc[4:0] }:2byte
+	TxHeaderOffset = 3
 )
 
 // External storage memory, available for connection to the controller's expansion port.
@@ -37,27 +40,38 @@ func NewCtrlPak(binPath string) (*CtrlPAK, error) {
 }
 
 // Do nothing
-// Data are not cleared because they are written on non-volatile memory or are backed up by batteries.
+// Data are not cleared because are backed up by batteries.
 func (pak *CtrlPAK) Reset() {
 	// do nothing
 }
 
 // Do Command
 func (pak *CtrlPAK) Run(cmd joybus.CommandType, txBuf, rxBuf []types.Byte) joybus.CommandResult {
-	// block offset not found
-	if len(txBuf) < nvmem.TxHeaderOffset {
-		assert.Assert(false, "block offset is not included in the sent data.")
+	// block offset and crc not found
+	if len(txBuf) < TxHeaderOffset {
+		assert.Assert(false, "block offset and crc are not included in the sent data.")
 		return joybus.UnableToTransferDatas
 	}
-	blockOffset := txBuf[1]
+	// parse args: { block_offset[15:5] ,crc[4:0] }
+	blockOffset := types.HalfWord((txBuf[1] << 3) | (txBuf[2] >> 5))
+	// crc := (txBuf[1] & 0x1f)
+
+	// Examine the CRC included in the command
+	util.TODO("examine the crc")
 
 	switch cmd {
 	case joybus.ReadFromMempackSlot:
-		return pak.RAM.Read(blockOffset, rxBuf)
+		// At the end of the readout data, the CRC is set to 1 byte
+		ret := pak.RAM.Read(blockOffset, rxBuf[:len(rxBuf)-1]) // without crc byte
+		util.TODO("check crc")
+		return ret
 	case joybus.WriteToMempackSlot:
-		return pak.RAM.Write(blockOffset, txBuf)
-
+		ret := pak.RAM.Write(blockOffset, txBuf[TxHeaderOffset:]) // without header
+		// If 1 is specified in rx, calculate and write the CRC to the Read result
+		util.TODO("check crc")
+		return ret
 	default:
+		// via the controller's accesory port, so Reset and Info are not accepted
 		assert.Assert(false, "Unsupported command")
 		return joybus.UnableToTransferDatas
 	}
